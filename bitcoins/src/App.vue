42937d29df825c94c85505a98263f7a53e9781c3
Mr.THANAPHON SOMBUNKAEO <62130500212@st.sit.kmutt.ac.th>
2020-03-18 15:48:37 +0700

    [UPDATE] bitcoins

--- bitcoins/src/App.vue
+++ bitcoins/src/App.vue
@@ -1,22 +1,64 @@
 <template>
-  <div id="app">
-    <Card />
-  </div>
+    <div id="app">
+        <h1>Bitcoin Price Index</h1>
+        <div v-if="error">Error for feting data from api</div>
+        <div v-else>
+            <div v-if="loading">
+                <p>Loading...</p>
+            </div>
+            <div v-else>
+                <div
+                    v-for="currency in info"
+                    :key="currency.description"
+                    class="currency"
+                >
+                    {{ currency.description }}:
+                    <span class="lighten">
+                        <span v-html="currency.symbol"></span
+                        >{{ currency.rate_float | currencydecimal }}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Card from './components/Card.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    Card
-  }
-}
+    name: 'App',
+    data() {
+        return {
+            info: null,
+            loading: true,
+            error: false
+        };
+    },
+    filters: {
+        currencydecimal(value) {
+            return value.toFixed(2);
+        }
+    },
+    mounted() {
+        axios
+            .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(response => {
+                this.info = response.data.bpi;
+            })
+            .catch(error => {
+                console.log(error);
+                this.error = true;
+            })
+            .finally(() => {
+                this.loading = false;
+            });
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 </style>
--- bitcoins/src/components/Card.vue
+++ bitcoins/src/components/Card.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    
+    <p>test card</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Card',
-  
+
 }
 </script>
 
