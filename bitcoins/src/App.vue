b5eefd37eeaba24c6abb0720b3ab11fef4f3583c
Mr.THANAPHON SOMBUNKAEO <62130500212@st.sit.kmutt.ac.th>
2020-03-18 14:56:00 +0700

    [ADD] bitcoins project

---
+++ bitcoins/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
