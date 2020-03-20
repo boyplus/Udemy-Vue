21b1e5a685e9a9645aa1c8153c206ef653a9283e
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-03-20 23:13:18 +0700

    [FINISH] monster project

--- Monster/app.js
+++ Monster/app.js
@@ -12,28 +12,46 @@ new Vue({
             this.you = 100;
             this.monster = 100;
         },
-        attack: function() {
-            const r1 = Math.floor(parseInt(Math.random() * 10)) + 1;
-            const r2 = Math.floor(parseInt(Math.random() * 10)) + 1;
+        attack: function(mx) {
+            const r1 = Math.floor(parseInt(Math.random() * mx)) + 1;
+            const r2 = Math.floor(parseInt(Math.random() * mx)) + 1;
             this.you -= r1;
             this.monster -= r2;
-            this.logs.push('MONSTER HITS PLAYER FOR ' + r1);
-            this.logs.push('PLAYER HITS MONSTER FOR ' + r2);
-        },
-        specialAttack: function() {
-            const r1 = Math.floor(parseInt(Math.random() * 15)) + 1;
-            const r2 = Math.floor(parseInt(Math.random() * 15)) + 1;
-            this.you -= r1;
-            this.monster -= r2;
-            this.hasLog = true;
+            this.logs.push({
+                msg: 'MONSTER HITS PLAYER FOR ' + r1,
+                playerTurn: false
+            });
+            this.logs.push({
+                msg: 'PLAYER HITS MONSTER FOR ' + r2,
+                playerTurn: true
+            });
+            this.checkWin();
         },
         heal: function() {
             const r2 = Math.floor(parseInt(Math.random() * 20)) + 1;
             this.you += 10 - r2;
             this.hasLog = true;
+            this.checkWin();
         },
         giveUp: function() {
             this.startgame = false;
+        },
+        checkWin: function() {
+            console.log('checked');
+            if (this.mosnter <= 0 && this.you <= 0) {
+                this.mosnter = 0;
+                this.you = 0;
+                this.startgame = false;
+                alert('draw');
+            } else if (this.monster <= 0) {
+                this.mosnter = 0;
+                this.startgame = false;
+                alert('You win');
+            } else if (this.you <= 0) {
+                this.you = 0;
+                this.startgame = false;
+                alert('You lose');
+            }
         }
     },
     computed: {
