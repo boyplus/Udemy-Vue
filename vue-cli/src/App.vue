235466612aae853ac1471a40464bc223c2b14018
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-03-21 22:09:25 +0700

    [UPDATE] vue-cli project

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Hello app</h1>
+  <app-servers></app-servers>
 </template>
 
 <script>
---
+++ vue-cli/src/ServerStatus.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <button @click="changeStatus">Click me</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      status: "Critical"
+    };
+  },
+  methods: {
+    changeStatus: function() {
+      this.status = "Normal";
+    }
+  }
+};
+</script>
