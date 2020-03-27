ffc5e7f1f3187cacc667e47c8e28c57fabfdfada
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-03-27 11:16:57 +0700

    [INIT] communicate project

---
+++ communicate/src/components/UserEdit.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="component" id="edit">
+        <h3>You may edit the User here</h3>
+        <p>Edit me!</p>
+    </div>
+</template>
+
+<script></script>
+
+<style scoped>
+#edit {
+    background-color: lightgreen;
+}
+</style>
