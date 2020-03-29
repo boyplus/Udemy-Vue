02b2e1ba2f26ff975fda75bf4e595c4a562b65fd
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-03-30 01:52:24 +0700

    [UPDATE] vue-http

--- vue-http/src/App.vue
+++ vue-http/src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Vue-HTTP</h1>
+                <div class="form-group">
+                    <label>Username </label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="user.username"
+                    />
+                </div>
+                <div class="form-group">
+                    <label>Email </label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        v-model="user.email"
+                    />
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            user: {
+                username: '',
+                email: ''
+            }
+        };
+    },
+    methods: {
+        submit() {
+            this.$http
+                .post(
+                    'https://vue-http-e0103.firebaseio.com/data.json',
+                    this.user
+                )
+                .then(
+                    response => {
+                        console.log(response);
+                    },
+                    error => {
+                        console.log(error);
+                    }
+                );
+        }
+    }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
