d841f65fab62bb9fbdcdb5edda06db3353d71bf7
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-03-29 22:03:29 +0700

    [UPDATE] filter project

--- filter/src/App.vue
+++ filter/src/App.vue
@@ -1,11 +1,48 @@
 <template>
-    <div id="app">
-      
+    <div id="app" class="container">
+        <div class="row">
+            <div class="col">
+                <p>{{ text | toUpperCase }}</p>
+                <p>{{ text | toLowerCase }}</p>
+                <h3>Normal rendered</h3>
+                <ul>
+                    <li v-for="fruit in fruits" :key="fruit">
+                        {{ fruit }}
+                    </li>
+                </ul>
+                <input v-model="filterText" />
+                <ul>
+                    <li v-for="fruit in filteredFruits" :key="fruit">
+                        {{ fruit }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            text: 'Hello World',
+            filterText: '',
+            fruits: ['Banana', 'Apple', 'Mango', 'Melon']
+        };
+    },
+    filters: {
+        toUpperCase(value) {
+            return value.toUpperCase();
+        }
+    },
+    computed: {
+        filteredFruits() {
+            return this.fruits.filter(element => {
+                return element.match(this.filterText);
+            });
+        }
+    }
+};
 </script>
 
 <style></style>
--- filter/src/main.js
+++ filter/src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.filter('toLowerCase', function(value) {
+    return value.toLowerCase();
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount('#app');
