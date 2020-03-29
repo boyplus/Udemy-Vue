26a9e530c96af59c91b939ed7d75aea55d137bc2
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-03-29 21:49:56 +0700

    [FINISH] directive project

--- directive/src/App.vue
+++ directive/src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container" id="app">
+        <div class="row">
+            <div>
+                <h3>The default directive</h3>
+                <p v-text="'hello directive'"></p>
+                <p v-html="'<strong>Some strong text</strong>'"></p>
+            </div>
+        </div>
+        <div class="row">
+            <div>
+                <h3>The custom directive</h3>
+                <p v-hilight:background.delayed="'red'">
+                    Hello custom directive
+                </p>
+                <p v-hilight:background="'pink'">Hello custom directive</p>
+                <p v-hilight="'blue'">Hello custom directive</p>
+
+                <p v-local-hilight:background="'pink'">
+                    Hello custom directive
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    directives: {
+        'local-hilight': {
+            bind(el, binding) {
+                let delay = 0;
+                if (binding.modifiers['delayed']) {
+                    delay = 3000;
+                }
+                setTimeout(() => {
+                    if (binding.arg == 'background') {
+                        el.style.backgroundColor = binding.value;
+                    } else {
+                        el.style.color = binding.value;
+                    }
+                }, delay);
+            }
+        }
+    }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- directive/src/main.js
+++ directive/src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.directive('hilight', {
+    bind(el, binding) {
+        let delay = 0;
+        if (binding.modifiers['delayed']) {
+          delay = 3000;
+        }
+        setTimeout(() => {
+            if (binding.arg == 'background') {
+                el.style.backgroundColor = binding.value;
+            } else {
+                el.style.color = binding.value;
+            }
+        }, delay);
+    }
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount('#app');
