08ca15635b55fab2aa4cf2a9320205344a59bd83
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-03-30 16:33:55 +0700

    [INIT] vue-router project

---
+++ note/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="intro">
+        <h1>{{ timestamp }}</h1>
+        <div id="app">
+            <span>{{ datenow }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+var moment = require('moment');
+export default {
+    data() {
+        return {
+            datenow: ''
+        };
+    },
+    methods: {
+        time() {
+            var self = this;
+            this.datenow = moment().format();
+            setTimeout(self.time, 1000);
+        }
+    },
+    mounted: function() {
+        this.time();
+    }
+};
+</script>
+
+<style></style>
