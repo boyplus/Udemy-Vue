5ae02af9d05678839b827c244a988c7f077232b8
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-04-01 03:02:41 +0700

    [ADD] vuex

---
+++ james/src/routes.js
@@ -0,0 +1,6 @@
+import Home from './components/Home.vue';
+import James from './components/James.vue';
+export const routes = [
+    { path: '', name: 'home', component: Home },
+    { path: '/james', name: 'james', component: James }
+];
